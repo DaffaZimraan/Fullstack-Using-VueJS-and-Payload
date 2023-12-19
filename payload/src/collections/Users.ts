dd3d90f5887aee2e34cea839bdd3f7bcf6f615be
DaffaZimraan <daffazimraan12@gmail.com>
2023-12-19 08:49:22 +0700

    Your commit message

---
+++ payload/src/collections/Users.ts
@@ -0,0 +1,20 @@
+import { CollectionConfig } from 'payload/types'
+
+const Users: CollectionConfig = {
+  slug: 'users',
+  auth: true,
+  admin: {
+    useAsTitle: 'email',
+  },
+  access:{
+    create: () => true,
+    read: () => true,
+    update: () => true,
+    delete: () => true
+  },
+  fields: [
+    
+  ],
+}
+
+export default Users
